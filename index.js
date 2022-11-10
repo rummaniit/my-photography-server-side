65f2b0bbd2f6d0420df02d27de398d432271e0c2
rummaniit <rummanoffice12@gmail.com>
2022-11-10 21:55:27 +0600

    jwt

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
+var jwt = require('jsonwebtoken');
 
 // calling
 const app = express()
@@ -17,6 +18,24 @@ const port = process.env.port || 5000
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xteb5ll.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+function verifyJWT(req, res, next) {
+    const authorize = req.headers.authorization
+    // let decoded=req.decoded
+    if (!authorize) {
+        res.status(401).send({
+            message: 'unauthorised access'
+        })
+    }
+    const token = authorize.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            res.status(403).send({ message: 'unauthorised access' })
+        }
+        req.decoded = decoded
+        next()
+    })
+}
 async function run() {
     await client.connect()
     console.log("Data is Connected");
@@ -99,14 +118,35 @@ async function run() {
         })
 
         app.post('/users', async (req, res) => {
-            let users = req.body
 
+            let users = req.body
             const result = await userCollections.insertOne(users);
             res.send(result)
         })
+        app.post('/jwt', (req, res) => {
+            // let decoded = req.decoded
+            // if (decoded.email != req.query.email) {
+            //     res.send({ message: 'Unauthorised ' })
+            // }
+            // const result = req.body
+            // var token = jwt.sign(result, process.env.ACCESS_TOKEN_SECREET, { expiresIn: '1h' });
+
+            // res.send({ token })
+            const user = req.body
+            var token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ token })
+            console.log(user);
+        })
 
 
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyJWT, async (req, res) => {
+            console.log(req.headers.authorization);
+            let decoded = req.decoded
+            if (decoded.email !== req.query.email) {
+                res.status(403).send({
+                    message: 'unauthorised access'
+                })
+            }
             let query = {}
             if (req.query.email) {
                 query = { email: req.query.email };
@@ -120,6 +160,7 @@ async function run() {
     finally {
         console.log('All is Looking god');
     }
+
 
 }
 run()
